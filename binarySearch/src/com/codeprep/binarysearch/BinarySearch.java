91952c3a3e480558ab01822eba38ba17c70720b0
thejavamonk <thejavamonk@gmail.com>
2018-01-07 23:17:11 +0530

    binary search implementation

---
+++ binarySearch/src/com/codeprep/binarysearch/BinarySearch.java
@@ -0,0 +1,40 @@
+package com.codeprep.binarysearch;
+
+import java.util.Scanner;
+
+public class BinarySearch {
+	
+	public static int search(int [] a, int n, int searchValue){
+		
+		return 0;
+	}
+
+	public static void main(String[] args) {
+		
+		int i, n, searchValue, index;
+		int [] a = new int [100];
+		
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter the number of elements : ");
+		n = scanner.nextInt();
+		
+		System.out.println("Enter the elements in a sorted order - ");
+		for(i = 0; i < n; i ++){
+			a[i] = scanner.nextInt();
+		}
+		
+		System.out.println("Enter the search value : ");
+		searchValue = scanner.nextInt();
+		
+		index = search(a, n, searchValue);
+		
+		if(index == -1){
+			System.out.println("Value "+searchValue +" not present in the array");
+		}
+		else{
+			System.out.println("Value "+searchValue+ " present at index "+index);
+		}
+		scanner.close();
+	}
+
+}
