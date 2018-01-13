ab24c9f742ea396ee69825ead113f6a1735e084f
thejavamonk <thejavamonk@gmail.com>
2018-01-13 22:01:39 +0530

    binary search implementation

--- binarySearch/src/com/codeprep/binarysearch/BinarySearch.java
+++ binarySearch/src/com/codeprep/binarysearch/BinarySearch.java
@@ -6,7 +6,23 @@ public class BinarySearch {
 	
 	public static int search(int [] a, int n, int searchValue){
 		
-		return 0;
+		int first = 0, last = n -1, mid;
+		
+		while(first <= last){
+			
+			mid = (first + last) / 2;
+			
+			if(searchValue < a[mid]){
+				last = mid - 1;
+			}
+			else if (searchValue > a[mid]){
+				first = mid + 1;
+			}
+			else{
+				return mid;
+			}
+		}
+		return -1;
 	}
 
 	public static void main(String[] args) {
